cc47edc732c5327fcf3e21183471b82ee2929488
vishalpawar17 <38483674+vishalpawar17@users.noreply.github.com>
2018-07-15 19:51:55 -0400

    tweets

--- src/main/java/com/tweets/service/TweetsService.java
+++ src/main/java/com/tweets/service/TweetsService.java
@@ -3,14 +3,14 @@ package com.tweets.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.tweets.entity.User;
 import com.tweets.exception.TweetsNotFoundException;
 import com.tweets.model.TweetsData;
 import com.tweets.repository.TweetsRepository;
 
-@Component
+@Service
 public class TweetsService {
 
 	@Autowired
